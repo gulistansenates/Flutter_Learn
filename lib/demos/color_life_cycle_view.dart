7bd3b51fd7e83ad765afc3bf79a26a7be59558cb
gulistansenates <95415294+gulistansenates@users.noreply.github.com>
2023-05-03 15:30:56 +0300

    feat: add color life cycle and demos

---
+++ lib/demos/color_life_cycle_view.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+import 'color_demos_view.dart';
+
+class ColorLifeCycleView extends StatefulWidget {
+  const ColorLifeCycleView({Key? key}) : super(key: key);
+
+  @override
+  State<ColorLifeCycleView> createState() => _ColorLifeCycleViewState();
+}
+
+class _ColorLifeCycleViewState extends State<ColorLifeCycleView> {
+  Color? _backgroundColor;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          IconButton(
+              onPressed: _changeBackground, icon: const Icon(Icons.clear))
+        ],
+      ),
+      body: Column(
+        children: [
+          const Spacer(),
+          Expanded(child: ColorDemos(initialColor: _backgroundColor))
+        ],
+      ),
+    );
+  }
+
+  void _changeBackground() {
+    setState(() {
+      _backgroundColor = Colors.pink;
+    });
+  }
+}
